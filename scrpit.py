8d5dab2c9d61c645b7db48acb32e6fa5cc55f834
sunandamahanta <75204704+sunandamahanta@users.noreply.github.com>
2021-08-30 17:04:24 +0100

    Add files via upload

---
+++ scrpit.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import urllib.request
+
+def url_to_jpg(i,url,file_path):
+	filename = 'image-{}.jpg'.format(i)
+	full_path = '{}{}'.format(file_path, filename)
+	urllib.request.urlretrieve(url, full_path)
+
+	print('{} saved'.format(filename))
+	return None
+
+FILENAME = 'Amazon_urls.xlsx'
+FILEPATH = 'images/'
+
+urls = pd.read_excel(FILENAME)
+for i,url in enumerate(urls.values):
+	url_to_jpg(i,url[0],FILEPATH)
